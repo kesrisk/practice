7df0ff573059165226cbd3083c85a54b05609fa4
Shivam Kesarwani <kesri.sk@gmail.com>
2021-04-14 08:20:53 +0530

    tow kknights

---
+++ shivam/Introductory Problems/Two Knights.py
@@ -0,0 +1,56 @@
+# https://cses.fi/problemset/task/1072
+def fact(n):
+    res = 1
+    for i in range(2, n + 1):
+        res = res * i
+    return res
+
+def calculateTotalCombinitionsPossible(totalPositions):
+    if totalPositions < 2:
+        return 0
+    return fact(totalPositions)//(fact(totalPositions - 2)*fact(2))
+
+def calculateAttackingPositions(boardDimensions, smallerBoardDimesnsionsAttackingPosition):
+    # we are considering 1st column and last row as outer/outside
+    attackingPositions = 0
+    for boardPosition in range(1, boardDimensions):
+        if boardPosition + 2 < boardDimensions:
+            # inside
+            attackingPositions += 2
+        if boardPosition + 2 == boardDimensions:
+            # outside
+            attackingPositions += 1
+
+        if boardPosition - 2 >= 1:
+            # always inside
+            attackingPositions += 2
+
+        if boardPosition - 1 >= 1:
+            # always inside
+            attackingPositions += 2
+
+        if boardPosition + 1 < boardDimensions:
+            # inside
+            attackingPositions += 2
+
+        if boardPosition + 1 == boardDimensions:
+            # outside
+            attackingPositions += 1
+
+    return (attackingPositions//2 * 2 + 2) + smallerBoardDimesnsionsAttackingPosition
+
+
+def calculateSafePositions(boardDimensions, totalAttackingPositionCountArray):
+    if boardDimensions <= 2:
+        attackingPostion = 0
+    else:
+        attackingPostion = calculateAttackingPositions(boardDimensions, totalAttackingPositionCountArray[-1])
+
+    totalAttackingPositionCountArray.append(attackingPostion)
+    return calculateTotalCombinitionsPossible(boardDimensions*boardDimensions) - attackingPostion
+
+
+n = int(input())
+totalAttackingPositionCountArray = []
+for i in range(1, n+1):
+    print(calculateSafePositions(i, totalAttackingPositionCountArray))
